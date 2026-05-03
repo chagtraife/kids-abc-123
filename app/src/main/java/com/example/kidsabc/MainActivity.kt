0324db3366c8a3e0f5f18e9451bd28875d5864c8
chagtraife <thang.nguyenngoc1996@gmail.com>
2026-05-04 00:16:00 +0700

    Set app to landscape orientation with fullscreen
    - Added android:screenOrientation="landscape" to MainActivity
    - Hide status bar and navigation bar for fullscreen experience
    - Use SYSTEM_UI_FLAG_IMMERSIVE_STICKY for immersive mode
    - Better for kids learning interface on tablets/phones

--- app/src/main/java/com/example/kidsabc/MainActivity.kt
+++ app/src/main/java/com/example/kidsabc/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kidsabc
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -26,6 +27,14 @@ import com.example.kidsabc.ui.feature.quiz.QuizScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide system UI for fullscreen experience
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
+
         setContent {
             KidsABCApp()
         }
